482a163dd6ebb3fbcff543ee809f96c3942d2177
AULA-106-12 <santiagobcuevas14@gmail.com>
2023-09-08 20:41:00 -0300

    Trabajo con arrays

--- Clase5Tareas/cola.py
+++ Clase5Tareas/cola.py
@@ -6,8 +6,8 @@ class cola :
         print(f'Cliente {x} agregado, cola actualizada: {self.clientes}')
     def es_vacia(self):
         return len(self.clientes) == 0
-    def desencolar(self):
-        print(f'Cliente {self.clientes.pop(0)} atendido, cola actualizada {self.clientes}')
+    def atender(self):
+        print(f'Cliente {self.clientes.pop(0)} atendido, cola actualizada ({len(self.clientes)}): {self.clientes}')
     def mostrar(self):
         print(f'Turnos restantes ({len(self.clientes)}): {self.clientes}')
 miCola = cola()
@@ -19,5 +19,10 @@ miCola.encolar("Jamaica")
 miCola.encolar("Cristobal")
 miCola.encolar("Returno")
 print(f'La cola está vacía: {miCola.es_vacia()}')
-miCola.desencolar()
+miCola.atender()
+miCola.atender()
+miCola.atender()
+miCola.atender()
+miCola.atender()
+miCola.atender()
 miCola.mostrar()
